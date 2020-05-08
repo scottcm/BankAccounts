56e9f0d57c9d7b11098313c61e7fe05e6ab2573c
fehrsean <62909622+fehrsean@users.noreply.github.com>
2020-05-08 13:37:59 -0500

    Sean changes to checkinaccount.cpp

--- BankAccounts/CheckingAccount.cpp
+++ BankAccounts/CheckingAccount.cpp
@@ -3,20 +3,94 @@
 // Constructor
 CheckingAccount::CheckingAccount(double balance, double apr, int suffixOffset) :
 	BankAccount(balance, apr, AccountType::Checkings, suffixOffset)
-{
-	// Checking account specific initialization here
-}
+    //contents of CheckingAccount.h
 
-// Override BankAccount::withdrawal
+#ifndef CHECKINGACCOUNT_H
+#define CHECKINGACCOUNT_H
 
+#include <iostream>
+#include "BankAccount.h"
 
-// Override BankAcccount::deposit
+    using namespace std;
 
+class CheckingAccount : public BankAccount {
+private:
+    //member variable to track what's owed to the bank
+    double owed;
 
-// Override BankAcccount::calcInt
+public:
+    //constructor, will call
+    //constructor of parent class
+    CheckingAccount(double b, double air) :
+        BankAccount(b, air)
+    {
+        owed = 0.0;
+    }
 
+    //accessor function
+    double getAmountOwed() const {
+        return owed;
+    }
 
-// Override BankAcccount::monthlyProc
+    //withdraw() function of the checking account
+    virtual void withdraw(double w) {
+        //to check if withdrawal with make account negative
+        if (w > balance) {
+            //print error message
+            cout << fixed << setprecision(2);
+            cout << "Unable to Withdraw! You attempted to withdraw $";
+            cout << w << " but current balance is $" << balance;
+            cout << ".\nYou will Overdraft and be charged $15.00 for this.\n";
 
+            //subtract 15 from balance
+            balance -= 15.0;
+        }
+        //else, perform withdrawal
+        else {
+            BankAccount::withdraw(w);
+        }
+    }
 
-// Other member functions
+    //monthlyProc() function of checking account
+    virtual ServAndOwed monthlyProc() {
+        //add fee to service charges
+        serviceCharges += 5.0;
+
+        //also add the charges per withdrawal
+        serviceCharges += 0.1 * numWithdrawal;
+
+        //add the service charges to monthly charges
+        mnCharges += serviceCharges;
+
+        //structure to return at the end, the service
+        //charges and total owed to bank, because
+        // they will be reset upon execution of
+        //BankAccount::monthlyProc()
+        ServAndOwed temp;
+        temp.servCharges = serviceCharges;
+
+        //perform monthly processing this resets 
+        //serviceCharges variable
+        BankAccount::monthlyProc();
+
+        //after processing, check for negative balance
+        if (balance < 0) {
+            //update owed member variable
+            //to amount owed to bank by user
+            owed = 0 - balance;
+        }
+
+        //store it in the structure
+        temp.totalOwed = owed;
+
+        //reset owed to 0
+        owed = 0;
+
+        //return the structure, from which can be
+        //retrieved the service charges of this month
+        return temp;
+    }
+
+};
+
+#endif
